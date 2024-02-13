5a0793a993859c1edeb9a4db7dd0bbfafab88830
AlphaFoxz <841958335@qq.com>
2024-02-13 14:39:28 +0800

    opt: better highlight in monaco-editor

--- src/components/restful-dsl-editor/restl.ts
+++ src/components/restful-dsl-editor/restl.ts
@@ -75,49 +75,140 @@ export function registerRestl(monaco: { editor: any; languages: any }) {
       'map',
       'void',
     ],
+    // tokenizer: {
+    //   root: [
+    //     [/\bnamespace\b/, { token: 'keyword', next: '@namespace_body' }],
+    //     [/[!|?]/, { token: 'keyword.operator.optional' }],
+    //     [
+    //       /[a-zA-Z][\w$]*/,
+    //       {
+    //         cases: {
+    //           '@keyword': 'keyword',
+    //           '@default': 'type',
+    //         },
+    //       },
+    //     ],
+    //     [/[0-9]+\s*:/, 'identifier'],
+    //     [/\"[^\"]*\"/, 'string'],
+
+    //     [/\/\/[\s]*@[^\(]+/, { token: 'tag', next: '@annoValue' }],
+    //     [/\/\/.*$/, 'comment'],
+    //     [/\/\*/, { token: 'comment.doc', next: '@commentDoc' }],
+    //   ],
+    //   annoValue: [
+    //     [/\(|,/, 'delemiter'],
+    //     [/\)/, { token: 'delemiter', next: '@pop' }],
+    //     [/[,]/, 'delemiter'],
+    //     [/[^(\(|\)|,)]*/, 'string'],
+    //   ],
+    //   commentDoc: [
+    //     [/\*\//, { token: 'comment.doc', next: '@pop' }],
+    //     [/[^*/]+/, 'comment.doc'],
+    //     [/[*/]/, 'comment.doc'],
+    //   ],
+    //   namespace_body: [
+    //     [/[a-zA-Z_\.]+\s*$/, { token: 'namespace', next: '@pop' }],
+    //     [/\b[a-zA-Z]+\b/, 'variable'],
+    //   ],
+    //   service_header: [
+    //     [/}/, '@pop'],
+    //     [/\/\/.*$/, 'comment'],
+    //     [/\/\*/, { token: 'comment.doc', next: '@commentDoc' }],
+
+    //     [/\b[a-zA-Z_][\w$]*/, 'type'],
+    //   ],
+    //   service_body: [],
+    // },
     tokenizer: {
       root: [
         [/\bnamespace\b/, { token: 'keyword', next: '@namespace_body' }],
-
-        [
-          /[a-zA-Z][\w$]*/,
-          {
-            cases: {
-              '@keyword': 'keyword',
-              '@default': 'type',
-            },
-          },
-        ],
-        [/[0-9]+\s*:/, 'identifier'],
-        [/\"[^\"]*\"/, 'string'],
-
-        [/\/\/[\s]*@[^\(]+/, { token: 'tag', next: '@annoValue' }],
+        [/\bimport\b/, { token: 'keyword', next: '@import_path' }],
+        [/\@[a-zA-Z]+/, { token: 'punctuation.definition.annotation.java' }],
+        [/\@[a-zA-Z]+(?=[(])/, { token: 'punctuation.definition.annotation.java', next: '@annotation_param' }],
+        [/\binterface\b/, { token: 'keyword', next: '@interface' }],
+        [/\bclass\b/, { token: 'keyword', next: '@class' }],
+        [/\benum\b/, { token: 'keyword', next: '@enum' }],
         [/\/\/.*$/, 'comment'],
-        [/\/\*/, { token: 'comment.doc', next: '@commentDoc' }],
-      ],
-      annoValue: [
-        [/\(|,/, 'delemiter'],
-        [/\)/, { token: 'delemiter', next: '@pop' }],
-        [/[,]/, 'delemiter'],
-        [/[^(\(|\)|,)]*/, 'string'],
-      ],
-      commentDoc: [
-        [/\*\//, { token: 'comment.doc', next: '@pop' }],
-        [/[^*/]+/, 'comment.doc'],
-        [/[*/]/, 'comment.doc'],
+        [/\/\*/, { token: 'punctuation.definition.string.begin', next: '@comment_doc' }],
       ],
       namespace_body: [
         [/[a-zA-Z_\.]+\s*$/, { token: 'namespace', next: '@pop' }],
         [/\b[a-zA-Z]+\b/, 'variable'],
       ],
-      service_header: [
-        [/}/, '@pop'],
-        [/\/\/.*$/, 'comment'],
-        [/\/\*/, { token: 'comment.doc', next: '@commentDoc' }],
-
-        [/\b[a-zA-Z_][\w$]*/, 'type'],
+      import_path: [[/\"[^\"]*\"/, { token: 'string', next: '@pop' }]],
+      annotation_param: [
+        [/[)]/, { token: 'keyword.operator', next: '@pop' }],
+        [/[(]/, { token: 'keyword.operator' }],
+        [/[a-zA-Z/{}]+/, { token: 'string' }],
       ],
-      service_body: [],
+      interface: [
+        [/[}]/, { token: 'operator', next: '@pop' }],
+        [/[{]/, { token: 'operator', next: '@interface_body' }],
+        [/\b[a-zA-Z]+\b/, { token: 'entity.name.class' }],
+      ],
+      interface_body: [
+        [/\@[a-zA-Z]+/, { token: 'punctuation.definition.annotation.java' }],
+        [/\@[a-zA-Z]+(?=[(])/, { token: 'punctuation.definition.annotation.java', next: '@annotation_param' }],
+        [/\s(?=[}])/, { token: 'operator', next: '@pop' }],
+        [/\bmap\b/, { token: 'support.type.builtin.ts', next: '@type_t' }],
+        [/\bset\b/, { token: 'support.type.builtin.ts', next: '@type_t' }],
+        [/\blist\b/, { token: 'support.type.builtin.ts', next: '@type_t' }],
+        [/\benum\b/, { token: 'support.type.builtin.ts', next: '@type_t' }],
+        [/\b(i16|i32|i64|short|int|long|boolean|string|binary|byte)\b/, { token: 'support.type.builtin.ts' }],
+        [/[a-zA-Z]+\.[a-zA-Z]+/, { token: 'entity.name.type' }],
+        [/[!|?]/, { token: 'keyword.operator.optional' }],
+        [/\/\/.*$/, 'comment'],
+        [/\/\*/, { token: 'punctuation.definition.string.begin', next: '@comment_doc' }],
+      ],
+      class: [
+        [/[}]/, { token: 'operator', next: '@pop' }],
+        [/[{]/, { token: 'operator', next: '@class_body' }],
+        [/\b[a-zA-Z]+\b/, { token: 'entity.name.class' }],
+      ],
+      class_body: [
+        [/(?=[}])/, { token: 'operator', next: '@pop' }],
+        [/\b@[a-zA-Z]+/, { token: 'punctuation.definition.annotation.java', next: '@annotation_param' }],
+        [/\bmap\b/, { token: 'support.type.builtin.ts', next: '@type_t' }],
+        [/\bset\b/, { token: 'support.type.builtin.ts', next: '@type_t' }],
+        [/\blist\b/, { token: 'support.type.builtin.ts', next: '@type_t' }],
+        [/\benum\b/, { token: 'support.type.builtin.ts', next: '@type_t' }],
+        [/\b(i16|i32|i64|short|int|long|boolean|string|binary|byte)\b/, { token: 'support.type.builtin.ts' }],
+        [/[!|?]/, { token: 'keyword.operator.optional' }],
+        [/\/\/.*$/, 'comment'],
+        [/\/\*/, { token: 'keyword.operator.assignment.compound.js', next: '@comment_doc' }],
+      ],
+      enum: [
+        [/[}]/, { token: 'operator', next: '@pop' }],
+        [/[{]/, { token: 'operator', next: '@enum_body' }],
+        [/\b[a-zA-Z]+\b/, { token: 'entity.name.class' }],
+      ],
+      enum_body: [
+        [/(?=[}])/, { token: 'operator', next: '@pop' }],
+        [/[a-zA-Z_]+\s*(?=[=])/, { token: 'constant' }],
+        [/(?<=[=])[0-9]*/, { token: 'constant.numeric' }],
+        [/\/\/.*$/, 'comment'],
+        [/\/\*/, { token: 'keyword.operator.assignment.compound.js', next: '@comment_doc' }],
+      ],
+      type: [
+        [/\bmap\b/, { token: 'support.type.builtin.ts', next: '@type_t' }],
+        [/\bset\b/, { token: 'support.type.builtin.ts', next: '@type_t' }],
+        [/\blist\b/, { token: 'support.type.builtin.ts', next: '@type_t' }],
+        [/\benum\b/, { token: 'support.type.builtin.ts', next: '@type_t' }],
+        [
+          /\b(i16|i32|i64|short|int|long|boolean|string|binary|byte)\b/,
+          { token: 'support.type.builtin.ts', next: '@pop' },
+        ],
+        [/\b[a-zA-Z]+(\.[a-zA-Z]+)?\b/, { token: 'entity.other.inherited-class', next: '@pop' }],
+        [/\s/, { token: 'keyword', next: '@pop' }],
+      ],
+      type_t: [
+        [/[>]/, { token: 'operator', next: '@pop' }],
+        [/[<]/, { token: 'operator', next: '@type' }],
+      ],
+      comment_doc: [
+        [/\*\//, { token: 'punctuation.definition.string.end', next: '@pop' }],
+        [/.*(?=\*\/)/, 'punctuation.definition.string.begin'],
+      ],
     },
   })
   monaco.editor.onDidCreateModel((model: any) => {
